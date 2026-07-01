d4093ab1e172aa9e1329f89def0993a176a87e4a
Zernexa <cvr.radu.r@gmail.com>
2026-07-01 17:32:39 +0300

    Add: design-hashset - submission-6

---
+++ Data Structures & Algorithms/design-hashset/submission-6.java
@@ -0,0 +1,28 @@
+class MyHashSet {
+    int[] v = new int[1000001];
+    
+    public MyHashSet() {
+    }
+    
+    public void add(int key) {
+        v[key] = 1;
+    }
+    
+    public void remove(int key) {
+        v[key] = 0;
+    }
+    
+    public boolean contains(int key) {
+        if (v[key] == 1) return true;
+        else return false;
+    }
+
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
