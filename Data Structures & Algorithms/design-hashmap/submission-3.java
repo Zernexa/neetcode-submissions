4be1ac8d872fd4afe5e03abc65338ad64433c592
Zernexa <cvr.radu.r@gmail.com>
2026-07-02 10:41:21 +0300

    Add: design-hashmap - submission-3

---
+++ Data Structures & Algorithms/design-hashmap/submission-3.java
@@ -0,0 +1,29 @@
+class MyHashMap {
+
+    final int n = 1000001;
+    int[][] hashMap = new int[2][n];
+    boolean init = false;
+
+    public MyHashMap() {
+        if (init == false){
+            for (int i = 0; i < n; i++){
+                hashMap[1][i] = -1;
+            }      
+            for (int i = 0; i < n; i++){
+                hashMap[0][i] = i;
+            } 
+        }
+    }
+    
+    public void put(int key, int value) {
+        hashMap[1][key] = value;
+    }
+    
+    public int get(int key) {
+        return hashMap[1][key];
+    }
+    
+    public void remove(int key) {
+        hashMap[1][key] = -1;
+    }
+}
