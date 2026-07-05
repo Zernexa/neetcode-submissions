e5a766fc04009ff43839ea522645a3e67879999c
Zernexa <cvr.radu.r@gmail.com>
2026-07-05 20:35:20 +0300

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        boolean okLeft = false, okRight = false;
+        int left = 0, right = s.length() - 1;
+        while(left < right){
+            while(!Character.isLetterOrDigit(s.charAt(right)) && left < right){
+                right--;
+            }
+            while(!Character.isLetterOrDigit(s.charAt(left)) && left < right){
+                left++;
+            }
+            
+            char cl = s.charAt(left), cr = s.charAt(right);
+
+            cl = Character.toLowerCase(cl);
+            cr = Character.toLowerCase(cr);
+
+            if (cl == cr){
+                left++;
+                right--;
+                continue;
+            } else return false;
+        }
+        return true;
+    }
+}
